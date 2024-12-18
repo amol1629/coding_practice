54e5cdc94dde38d490144761d446e05957a2fa6d
amol <amol1629rathod@gmail.com>
2024-12-18 11:08:59 +0530

    code is now migrated to Amol's machine

--- react_practice/04reactHooks/01useStateHook/src/App.jsx
+++ react_practice/04reactHooks/01useStateHook/src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import UseStateHook from "./UseStateHook";
+import UseStateHook from "./components/UseStateHook";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
+    <div>
       <UseStateHook />
-    </>
+    </div>
   );
 }
 
--- react_practice/04reactHooks/01useStateHook/src/components/UseStateHook.jsx
+++ react_practice/04reactHooks/01useStateHook/src/components/UseStateHook.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 const UseStateHook = () => {
   const [color, setColor] = useState(""); // For dynamic background color
