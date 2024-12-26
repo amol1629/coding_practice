2b18b05eff804f3ea9649018b53d4768ac33b450
amol <amol1629rathod@gmail.com>
2024-12-26 23:25:00 +0530

    new project for useContext Hook is created.

---
+++ react_practice/04reactHooks/01usecontexthook/src/components/Home.jsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <div className="p-4 transition-all duration-300 ease-in-out bg-purple-100 border border-purple-500 shadow-xl rounded-xl hover:bg-purple-200">
+      <div>
+        <p className="text-3xl font-bold text-purple-500">
+          useContext Hook In REACT
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
