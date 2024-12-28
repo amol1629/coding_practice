2a4479b9f35c0ecea8e4d91b00b985a37eb8a2eb
Tushar Pawar <amol1629rathod@gmail.com>
2024-12-28 19:20:14 +0530

    Some of the JavaScript Interview Problems are done now

--- basic_of_javascript/index.js
+++ basic_of_javascript/index.js
@@ -28,33 +28,63 @@
 // fn();
 // fn();
 
-const varialbeTrue1 = "true";
-const varialbeTrue2 = true;
+// Data types comparison
+// const varialbeTrue1 = "true";
+// const varialbeTrue2 = true;
 
-const variableNumber1 = "21";
-const variableNumber2 = 21;
+// const variableNumber1 = "21";
+// const variableNumber2 = 21;
 
-console.log("Double Equals To For True: ", varialbeTrue1 == varialbeTrue2);
-console.log("Triple Equals To For True: ", varialbeTrue1 === varialbeTrue2);
+// console.log("Double Equals To For True: ", varialbeTrue1 == varialbeTrue2);
+// console.log("Triple Equals To For True: ", varialbeTrue1 === varialbeTrue2);
 
-console.log(
-  "Double Equals To For Number 21 : ",
-  variableNumber1 == variableNumber2
-);
-console.log(
-  "Triple Equals To For Number 21 : ",
-  variableNumber1 === variableNumber2
-);
+// console.log(
+//   "Double Equals To For Number 21 : ",
+//   variableNumber1 == variableNumber2
+// );
+// console.log(
+//   "Triple Equals To For Number 21 : ",
+//   variableNumber1 === variableNumber2
+// );
 
-const person1 = {
-  name: "Tony",
-  age: 23,
-};
+// Object Comparison
+// const person1 = {
+//   name: "Tony",
+//   age: 23,
+// };
 
-const person2 = {
-  name: "Tony",
-  age: 23,
-};
+// const person2 = {
+//   name: "Tony",
+//   age: 23,
+// };
 
-console.log("Person1 == person2 : ", person1.name == person2.name);
-console.log("Person1 === person2 : ", person1.name === person2.name);
+// console.log("Person1 == person2 : ", person1.name == person2.name);
+// console.log("Person1 === person2 : ", person1.name === person2.name);
+
+// Difference between * (Multiplication) and ** (Exponentiation)
+// let a = 3;
+
+// Multiplication (*): Performs straightforward multiplication of two numbers.
+// console.log("a * 12 : ", a * 3);
+
+// Exponentiation (**): Raises a number (base) to the power of another number (exponent).
+// console.log("a ** 12 : ", a ** 3);
+
+// var a = {};
+// var b = { key: "b" };
+// var c = { key: "c" };
+
+// a[b] = 600;
+// b[c] = 700;
+
+// console.log(a[c]);
+// console.log(a[b]);
+// console.log(b[b]);
+// console.log(b[c]);
+
+
+
+
+
+console.log('5' ** '3')
+console.log('5' - 3)
