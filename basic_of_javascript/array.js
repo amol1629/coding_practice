f53d413cb5e4ca34258558636779c9386d5d25e8
Tushar Pawar <amol1629rathod@gmail.com>
2024-12-29 00:22:23 +0530

    Basic of Arrays is done now

--- basic_of_javascript/array.js
+++ basic_of_javascript/array.js
@@ -1,1 +1,7 @@
-console.log("hello arrays")
+let heroes = ["ironman", "hulk", "thod", "antman", "spiderman", "shaktiman"];
+
+// for of loop in array :
+for (let index in heroes) {
+  console.log(index, heroes[index]); // Outputs array indices
+  //   console.log(heroes[index]); // Access elements using the index
+}
