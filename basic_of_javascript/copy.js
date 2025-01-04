452f48ca366f46807c72e614538d7afca273ad69
amol <amol1629rathod@gmail.com>
2025-01-04 21:45:45 +0530

    working on shallow and deep copy

---
+++ basic_of_javascript/copy.js
@@ -0,0 +1,9 @@
+/*****************************************************************************************************************************************************
+ * * Shallow Copy in  JavaScript :
+ *  - The shallow copy of an object or array copies only the top-level properties or elements.
+ *  - If the object or array contains nested objects or arrays, the shallow copy will only copy the references to these nested objects or arrays, not the objects or arrays themselves. This means changes to nested objects in copied structure will reflect in the original.
+ * 
+ * *** Shallow Copy for Arrays ***
+ *  - In a shallow copy of an array, the elements of the original array are copied by reference for objects or arrays;
+ *  -
+ ******************************************************************************************************************************************************/
