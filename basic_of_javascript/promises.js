18eaf9f993a58900294f9239bf07a0dd9ca124c7
amol <amol1629rathod@gmail.com>
2025-01-10 00:19:47 +0530

    worked on promises little bit

--- basic_of_javascript/promises.js
+++ basic_of_javascript/promises.js
@@ -1,24 +1,47 @@
 /**
  * * Promises in JavaScript :
- *  - 
+ *  -
  */
 
-const promiseFunction = new Promise((resolve, reject) => {
-  let randomNumber = Math.floor(Math.random() * 10);
+// Basic Promise Example
+// const promiseFunction = new Promise((resolve, reject) => {
+//   let randomNumber = Math.floor(Math.random() * 10);
 
-  console.log("Random Number is : ", randomNumber);
+//   console.log("Random Number is : ", randomNumber);
 
-  if (randomNumber < 5) {
-    resolve();
-  } else {
-    reject();
-  }
-});
+//   if (randomNumber < 5) {
+//     resolve();
+//   } else {
+//     reject();
+//   }
+// });
 
-promiseFunction
-  .then(() => {
-    console.log("TRUE...");
+// promiseFunction
+//   .then(() => {
+//     console.log("TRUE...");
+//   })
+//   .catch(() => {
+//     console.log("FALSE...");
+//   });
+
+// Fetch Example :
+
+const GITHUB_URL = "https://api.github.com/users/amol1629";
+
+const fetchUser = fetch(GITHUB_URL);
+
+fetchUser
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
   })
-  .catch(() => {
-    console.log("FALSE...");
+  .then((data) => {
+    
+    console.log("User Data : ", data);
+    console.log("User Name : ", data.name);
+  })
+  .catch((error) => {
+    console.log("Error fetching user data : ", error);
   });
