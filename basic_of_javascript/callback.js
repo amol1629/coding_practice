145cf4796ac99f2d3cc0f2b43b80daeb76e58bdf
amol <amol1629rathod@gmail.com>
2025-01-14 15:38:55 +0530

    Callback Starvation and Inversion of Control is added to "callback.js" file.

--- basic_of_javascript/callback.js
+++ basic_of_javascript/callback.js
@@ -3,6 +3,7 @@
  *
  *  - Callbacks are the functions that are passed as arguments to other functions and are executed after the completion of those functions.
  *  - Callbacks are used to handle asynchronous operations , such as reading files, making network requests, or performing database operations and they enable non-blocking code execution by deferring tasks until a later time.
+ *
  *  - There are two ways in which the callback may be called:
  * 				1) Synchronous callbacks  : They are called immediately after the invocation of the outer function, with no intervening asynchronous tasks,
  * 				2) Asynchronous callbacks : They  are called at some point later, after an asynchronous operation has completed.
@@ -19,7 +20,26 @@
  *          1) Callbacks can lead to callback hell.
  *          2) Callbacks can be difficult to debug.
  *          3) Callbacks can lead to memory leaks.
+ * 			4) It leads to inversion of control.
  *
+ *  - Best Practices for Callback :
+ *			1) Avoid using callbacks for synchronous operations.
+ *			2) Handle errors properly.
+ *			3) Avoid nesting callbacks.
+ *			4) Use named functions instead of anonymous functions.
+ *
+ * 	- Callback Hell is also known as "Pyramid of Doom".
+ *
+ *  ? Callback Starvation :
+ *  - In JavaScript, starvation of the callback queue i.e "callback starvation" refers to a situation where the event loop keeps processing tasks from the microtask queue continuously, without giving  time for tasks in the callback queue to execute. This leads to starvation of the callback queue, meaning those callbacks do not get a chance to run.
+ * 
+ *  ? Inversion of Control : 
+ *  - Inversion of control is a design principle in which the control of the program is transferred to a framework or runtime environment. In the case of callbacks, the control is transferred to the function that calls the callback. 
+ *  - This can make the code harder to understand and maintain. 
+ *  - Inversion of control can also make it difficult to handle errors and exceptions. 
+ *  - To avoid inversion of control, you can use promises or async/await instead of callbacks.
+ * 	- Inversion of control is a common issue with callback-based programming, as the callback function is called by the system rather than by the developer.
+ * 	
  * -----------------------------------------------------------------------------------------------------
  */
 
