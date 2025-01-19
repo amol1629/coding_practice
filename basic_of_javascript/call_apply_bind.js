c4d58897d7d64147c609ac603b8dd47b27fb4b89
amol <amol1629rathod@gmail.com>
2025-01-19 23:51:28 +0530

    debounce is done now.

--- basic_of_javascript/call_apply_bind.js
+++ basic_of_javascript/call_apply_bind.js
@@ -10,18 +10,9 @@
  *
  *  - We can invoke the call() method immediately by passing individual arguments and it does not returns a new function like bind();
  *  - The call() borrow a method from one object to another.
- */
-
-// function describeRoleForCallMethod(role) {
-//   console.log(`${this.name} works as a ${role}`);
-// }
-
-// const employee = { name: "Tony" };
-
-// describeRoleForCallMethod.call(employee, "Front End Developer");
-
-/**
- * * Apply() Method :
+ *
+ *
+ *  * Apply() Method :
  *  - The apply() method is similar to call() method, but it takes an array (array-like object) of arguments instead of listing them individually.
  *
  *  - Syntax :
@@ -32,17 +23,9 @@
  *
  *  - We can invoke the apply() method immediately by passing  arguments in array and it does not returns a new function like bind();
  *  - Use apply() method when arguments are already in an array or array-like object.
- */
-
-// function describeRoleForApplyMethod(role, organization) {
-// 	console.log(`${this.name} works as a ${role} in ${organization}.`);
-// }
-
-// const employee = { name: "Tony" };
-
-// describeRoleForApplyMethod.apply(employee, ["Front End Developer", "Google"]);
-
-/**
+ *
+ *
+ *
  * * Bind() Method :
  *  - The bind() method returns a new function with a specific this context and optional arguments pre-applied, without immediately calling the function.
  *
@@ -54,17 +37,57 @@
  *
  *  - In bind() method, we can not call it immediately, we pass arguments to it individually and it returns a new bound function.
  *  - The bind() method creates a function with a fixed "this" context, useful for event handling or partial application.
+ *
+ *  ? Function Borrowing :
+ * 			=> Function borrowing allows an object to use the method belonging to another object.
+ * 			=> Function borrowing is commonly  done using call(), apply(), or bind(), which explicitly set the "this" context of a function to another object.
+ *
+ *  - The bind() method is most commonly used for currying since it creates a partially applied function. The call() and apply() methods are less frequently applied in this context but can still illustrate partial application.
+ *
+ *  -
  */
 
-function describeRoleForBindMethod(role, organization) {
-	console.log(`${this.name} works as a ${role} in ${organization}.`);
+// call() method example :
+// const person = {
+// 	name: "John",
+// 	age: 30,
+// 	introduce: function (organization, role) {
+// 		console.log(
+// 			`My name is ${this.name} and I am ${this.age} years old. I work in ${organization}, as a ${role}.`
+// 		);
+// 	},
+// };
+
+// const person1 = {
+// 	name: "Johnny",
+// 	age: 25,
+// };
+
+// person.introduce.call(person1, "Meta", "Software Engineer");
+
+// apply() method example :
+// const person = {
+// 	name: "John",
+
+// 	introduce: function (organization, role, age) {
+// 		console.log(
+// 			`My name is "${this.name}" and I am "${age}" years old. I work in "${organization}", as a "${role}".`
+// 		);
+// 	},
+// };
+
+// const person1 = {
+// 	name: "Mike",
+// 	age: 25,
+// };
+
+// person.introduce.apply(person1, ["Google", "Data Analyst", 25]);
+
+// bind() method example :
+
+function multiply(a, b) {
+	return a * b;
 }
 
-const employee = { name: "Tony" };
-
-const boundFunction = describeRoleForBindMethod.bind(
-	employee,
-	"Front End Developer",
-	"Google"
-);
-boundFunction();
+const multiplyByTwo = multiply.bind(null, 2); // Pre-set `a` to 2
+console.log(multiplyByTwo(5)); // Output: 10
--- basic_of_javascript/debounce.js
+++ basic_of_javascript/debounce.js
@@ -1,5 +1,65 @@
 /**
- *  * Debounce In JavaScript : 
- * 
- *  - 
+ *  * Debouncing In JavaScript :
+ *
+ *  - Debouncing in JavaScript is a technique used to limit the rate at which a function is executed, especially in response to events that occur frequently. It ensures that a function a function is only called after a certain period of inactivity, even if the event continues to fire multiple times within that period.
+ *
+ *  - Many events, such as scroll, resize, and input, can trigger repeatedly in a short span. If a function is executed for each event occurrence, it may lead to performance issues. Debouncing helps manage this by delaying the function call until after a specified period of time.
+ *
+ *  - How Debouncing Works :
+ *          1) Delay Timer : A time used to delay the execution of the function. Each time the event fires, the timer is reset.
+ *          2) Function Execution : The function executes only when the timer completes the delay without being reset again by subsequent events.
+ *
+ *  - When to use Debouncing :
+ *          1) Search Input Fields : Reducing API calls as users type.
+ *          2) Resize Events : Limiting layout recalculations when resizing the browser window.
+ *          3) Scroll Events : Preventing excessive API calls when scrolling through a long list.
+ *          4) Button Clicks : Avoiding multiple form submissions when clicking a button rapidly.
+ *
+ *  - Advantages of Debouncing :
+ *          1) Performance Improvement
+ *          2) Resource Efficiency
+ *          3) Improves User Experience
+ *
+ *  - Disadvantages of Debouncing :
+ *          1) May cause delays in function execution.
+ *          2) Can be complex to implement.
+ *
+ *
  */
+
+const inputField = document.getElementById("input-box");
+
+const DUMMY_PRODUCT_API = `https://dummyjson.com/products`;
+
+// Debouncing Function :
+const debounce = (fnc, delay = 500) => {
+	let timer;
+
+	return (...args) => {
+		clearTimeout(timer);
+
+		timer = setTimeout(() => {
+			fnc.apply(this, args);
+		}, delay);
+	};
+};
+
+const handleInputChange = async (e) => {
+	try {
+		const value = e.target.value;
+		console.log("Input : ", value);
+		const response = await fetch(
+			`https://dummyjson.com/products/search?q=${value}`
+		);
+
+		const result = await response.json();
+
+		console.log("Search Result : ", result);
+	} catch (error) {
+		console.error("Error while fetching products : ", error);
+	}
+};
+
+const debounced = debounce(handleInputChange, 500);
+
+inputField.addEventListener("input", debounced);
