a0c5177022f49c0f2c78af82caa04d025936a61e
amol <amol1629rathod@gmail.com>
2025-05-01 16:23:54 +0530

    Context API is finished now. Moving to Redux.

--- react_practice/06redux/01basicredux/src/contexts/theme/ThemeContext.js
+++ react_practice/06redux/01basicredux/src/contexts/theme/ThemeContext.js
@@ -1,4 +1,12 @@
-// Create a theme context
+/**
+ *  ? Context API :
+ *      1) Create Context using createContext() method.
+ *      2) Create a provider in which you can add states and method for global access.
+ *      3) Use that created context (using useContext() method) to access the states and methods in any component.
+ *      4) Wrap the provider around the component tree where you want to access the context.
+ *      5) Create your custom hook to access the context easily. (optional)
+ *
+ */
 
 import { createContext } from 'react'
 
